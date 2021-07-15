447a7c7c1d426494785f15d00386c51080a2b837
drgdiegoruiz <drgdiegoruiz@gmail.com>
2021-07-16 01:39:03 +0200

    Functionality implementation

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -5,10 +5,12 @@ calc = Tk(className='Calculator')
 #Define GUI size and lock it to that size
 calc.geometry("320x421")
 calc.resizable(0,0)
-#Define variable for the display
-resultext="" 
+#Define global variables
+resultext=""
+operation="" 
 #Define GUI elements
-result = Label(calc,text="", height=4,width=44)
+display1 = Label(calc, text="", height=2, width=44)
+display2 = Label(calc,text="", height=2,width=44)
 nine = Button(calc,text="9", width=10, height=4,command=lambda: addnum("9"))
 eight = Button(calc,text="8", width=10, height=4,command=lambda: addnum("8"))
 seven = Button(calc,text="7", width=10, height=4,command=lambda: addnum("7"))
@@ -32,41 +34,72 @@ equal = Button(calc, text="=",width=44, height=4,command=lambda: calculate())
 
 def addnum(x):
     global resultext
-    global result
-    resultext = result.cget("text")+x
-    result.configure({"text": resultext})
+    global display1
+    content = str(display1.cget("text")).count(".")
+    if x == "." and content == 0:
+        charnum=len(display1.cget("text"))
+        if charnum == 0 :
+            resultext = "0"+x
+            display1
+    elif x != ".":
+        resultext = display1.cget("text")+x
+        display1.configure({"text": resultext})
 
 
 def clear():
-    global result
-    result.configure({"text": ""})
+    global display1
+    display1.configure({"text": ""})
 
+def operator(x):
+    global display1
+    global display2
+    global operation
 
-def operator(o):
-    global result
+    display2.configure({"text": display1.cget("text")})
+    display1.configure({"text": ""})
+    operation=x
+
 
 def calculate():
-    global result
+    global operation
+    if operation == "+":
+        result=int(display1.cget("text")) + int(display2.cget("text"))
+        display1.configure({"text": result})
+        display2.configure({"text": ""})
+    if operation == "-":
+        result = int(display1.cget("text")) - int(display2.cget("text"))
+        display1.configure({"text": result})
+        display2.configure({"text": ""})
+    if operation == "x":
+        result = int(display1.cget("text")) * int(display2.cget("text"))
+        display1.configure({"text": result})
+        display2.configure({"text": ""})
+    if operation == "/":
+        result = int(display2.cget("text")) / int(display1.cget("text"))
+        display1.configure({"text": result})
+        display2.configure({"text": ""})
 
 #Place previus elements inside GUI grid
-result.grid(row="0", column="0", columnspan="4")
-nine.grid(row="1", column="0")
-eight.grid(row="1", column="1")
-seven.grid(row="1", column="2")
-six.grid(row="2", column="0")
-cinco.grid(row="2", column="1")
-four.grid(row="2", column="2")
-three.grid(row="3", column="0")
-two.grid(row="3", column="1")
-one.grid(row="3", column="2")
-zero.grid(row="4", column="1")
-decimal.grid(row="4", column="2")
-delete.grid(row="4", column="0")
-sum.grid(row="1", column="3")
-subtract.grid(row="2", column="3")
-multiply.grid(row="3", column="3")
-divide.grid(row="4", column="3")
-equal.grid(row="5", column="0", columnspan="4")
-print(result.cget("text"))
+display1.grid(row="0", column="0", columnspan="4")
+display2.grid(row="1", column="0", columnspan="4")
+nine.grid(row="2", column="0")
+eight.grid(row="2", column="1")
+seven.grid(row="2", column="2")
+six.grid(row="3", column="0")
+cinco.grid(row="3", column="1")
+four.grid(row="3", column="2")
+three.grid(row="4", column="0")
+two.grid(row="4", column="1")
+one.grid(row="4", column="2")
+zero.grid(row="5", column="1")
+decimal.grid(row="5", column="2")
+delete.grid(row="5", column="0")
+sum.grid(row="2", column="3")
+subtract.grid(row="3", column="3")
+multiply.grid(row="4", column="3")
+divide.grid(row="5", column="3")
+equal.grid(row="6", column="0", columnspan="4")
+print(display1.cget("text"))
+print(display2.cget("text"))
 #Start GUI
 calc.mainloop()
